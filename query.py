cf6a398b259d469678d40baa8dc93b07c7be0914
Brian Mwadime <brian.mwadime@gmail.com>
2016-03-06 23:57:09 +0300

    updates

--- query.py
+++ query.py
@@ -5,7 +5,7 @@ class Query(object):
 
     """The Query class constructs the URL params for a request.
 
-    :param api: The :class:`NassApi <nass.api.NassApi>` object
+    :param api: The :class:`USDAApi <nassusda.api.USDAApi>` object
     """
 
     def __init__(self, api):
@@ -14,21 +14,6 @@ class Query(object):
 
     def filter(self, param, value, op=None):
         """Apply a filter to the query.
-
-        Returns the :class:`Query <nass.query.Query>` object, so this
-        method is chainable.
-
-        The following code:
-          >>> q.filter('commodity_desc', 'CORN')
-          >>> q.filter('year', 2012)
-        is equivalent to this code:
-          >>> q.filter('commodity_desc', 'CORN').filter('year', 2012)
-
-        :param param: Parameter name to filter
-        :param value: Value to test against
-        :param op: (optional) Operator comparing param and value
-        :return: :class:`Query <nass.query.Query>` object
-        :rtype: nass.query.Query
         """
         if op is None:
             self.params[param] = value
@@ -41,15 +26,12 @@ class Query(object):
 
     def count(self):
         """Pass count request to :class:`NassApi <nass.api.NassApi>`.
-
         :return: The number of rows in the result
-        :rtype: int
         """
         return self.api.count_query(self)
 
     def execute(self):
-        """Pass query along to :class:`NassApi <nass.api.NassApi>`.
-
+        """Pass query along to :class:`USDAApi <nassusda.api.USDAApi>`.
         :return: The results of the query
         """
         return self.api.call_query(self)
